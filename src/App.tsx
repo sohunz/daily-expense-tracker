61e2b5a87fea45610426e2f4aaabf77a4414554f
sohunz <sohunily@gmail.com>
2024-06-04 22:52:18 +0700

    initail commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+    return (
+        <div>
+            <Button variant="outline">App</Button>
+        </div>
+    );
+};
+
+export default App;
