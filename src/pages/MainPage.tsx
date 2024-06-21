ab4dfa09a8807c7a0c02270010f0ca423c34c870
sohunz <sohunily@gmail.com>
2024-06-21 23:19:06 +0700

    add total expense on homepage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,12 +4,17 @@ import { useAddExpense } from "@/store/store";
 const MainPage = () => {
     const { expense } = useAddExpense((state) => state);
 
+    const totalExpense = expense.reduce(
+        (accumulator, currentItem) => accumulator + currentItem.value,
+        0
+    );
+
     return (
         <main className="w-full flex flex-col items-center justify-center h-full gap-20">
             <p className="flex flex-col items-center gap-4">
                 <span className="text-sm text-gray-500">Spent this month</span>
                 <span className="text-5xl font-semibold text-red-500">
-                    $ -200
+                    $ -{totalExpense}
                 </span>
             </p>
             <div className="w-full p-5 space-y-5">
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,7 @@ import { ExpenseType } from "@/types/types";
 import { create } from "zustand";
 
 export const useAddExpense = create<ExpenseType>((set) => ({
-    expense: [{ id: 1, title: "Buy Coffee", icon: "🍵", value: 250 }],
+    expense: [{ id: 1, title: "Buy Coffee", icon: "🍵", value: 12 }],
     addExpense: (value: number, icon: string, title: string) =>
         set((state) => ({
             expense: [
