716b41712e5c88085ed4cef1864ebf49158c4eb8
sohunz <sohunily@gmail.com>
2024-06-25 20:27:15 +0700

    change some

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
 import MainPage from "./MainPage";
 
 const HomePage = () => {
-    return <MainPage />;
+    return (
+        <div>
+            <MainPage />
+        </div>
+    );
 };
 
 export default HomePage;
